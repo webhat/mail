a0172110595c845425ed5589995b5d2c6690a3ee
webhat <daniel.crompton@gmail.com>
2013-03-20 13:03:04 +0000

    bugfix

--- src/php/Mail.class.php
+++ src/php/Mail.class.php
@@ -10,6 +10,7 @@ include_once "bootstrap.php";
 
 class Mail {
 	function send( $u, $mailbody = array()) {
+
 		$c = new Config();
 
 		$fullname = $u['name'];
@@ -28,6 +29,7 @@ class Mail {
 			throw new MailException("No mail body");
 		//$subject = "Test";
 		$subject = $c->service["name"] .": ";
+		$mailbody['body'] = "";
 
 		$arr = array(
 				"FNAME" => $fullname,
@@ -46,24 +48,44 @@ class Mail {
 				$c->mail['host'],
 				$c->mail['port']
 			);
+
 		$transport->setUsername($c->mail['user']);
 		$transport->setPassword($c->mail['pass']);
 		$swift = Swift_Mailer::newInstance($transport);
+
+	//	$debug = true;
+
+		if($debug == true) {
+			// To use the ArrayLogger
+			$logger = new Swift_Plugins_Loggers_ArrayLogger();
+			$swift->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
+
+			// Or to use the Echo Logger
+			$logger = new Swift_Plugins_Loggers_EchoLogger();
+			$swift->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
+		}
 
 		$message = new Swift_Message($subject);
 		$message->setFrom($from);
 		$message->setBody($html, 'text/html');
 		$message->setTo($to);
-//		$message->addPart($text, 'text/plain');
+		//$message->addPart($text, 'text/plain');
 		$headers = $message->getHeaders();
 		$headers->addTextHeader('X-MC-MergeVars', $json);
-		$headers->addTextHeader('X-MC-Template', $c->mail['template']);
+		//$headers->addTextHeader('X-MC-Template', $c->mail['template']);
+
+		if($debug == true) {
+			echo $logger->dump();
+		}
 
 		if ($recipients = $swift->send($message, $failures)) {
 			return true;
 		} else {
-			return false;
 			print_r($failures);
+			return false;
+		}
+		if($debug == true) {
+			echo $logger->dump();
 		}
 	}
 }
